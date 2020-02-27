7328676f9ce2caa2c9e9121f0e3342744e70c10c
Steve <ycsteven.tsao@gmail.com>
2020-02-27 12:17:32 +0800

    createRef 用法

--- src/Input.js
+++ src/Input.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 
 class Input extends Component {
+    myInput = createRef();
+
     componentDidMount() {
-        this.myInput.focus();
+        this.myInput.current.focus();
     }
 
     setRef = input => {
@@ -12,7 +14,7 @@ class Input extends Component {
         return (
             <div>
                 <h3>Enter your name</h3>
-                <input text="text" ref={this.setRef} />
+                <input text="text" ref={this.myInput} />
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Counter from './Counter';
+// import Counter from './Counter';
 import Input from './Input';
 
 render(<Input />, document.getElementById('root'));
